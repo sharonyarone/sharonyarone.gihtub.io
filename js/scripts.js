13f64c29c0460c89fc8858c5676c592b22f23d81
sharonyarone <168216402+sharonyarone@users.noreply.github.com>
2024-04-27 20:24:44 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -498,4 +498,35 @@ var MD5 = function (string) {
         var audio = new Audio('./music.mp3');
         // Lecture de la musique
         audio.play();
+
+
+        //confettis
+            const defaults = {
+              spread: 360,
+              ticks: 100,
+              gravity: 0,
+              decay: 0.94,
+              startVelocity: 30,
+              shapes: ["heart"],
+              colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+            };
+          
+            confetti({
+              ...defaults,
+              particleCount: 50,
+              scalar: 2,
+            });
+          
+            confetti({
+              ...defaults,
+              particleCount: 25,
+              scalar: 3,
+            });
+          
+            confetti({
+              ...defaults,
+              particleCount: 10,
+              scalar: 4,
+            });
+          
     });
